05048b491355aa7803c5a8a543881581ab88c85a
John Russell <jrussell@casteliasystems.com>
2022-04-10 21:14:31 -0400

    revised code and removed unused import in functions.py

--- common/functions.py
+++ common/functions.py
@@ -1,6 +1,6 @@
 import xlsxwriter
 import json
-import os, errno
+import os
 import pandas as pd
 from datetime import datetime
 
@@ -11,27 +11,32 @@ PROJECT_DIRECTORY = os.path.dirname(os.path.realpath(__file__))
 OUTPUT_DIRECTORY = os.path.abspath(os.path.join(PROJECT_DIRECTORY, os.pardir))
 OUTPUT_DIRECTORY = os.path.join(OUTPUT_DIRECTORY, "output")
 
-def isDeductable(text:str) -> bool:
+
+def isDeductable(text: str) -> bool:
     return False
 
-def extract_tags(text:str) -> str:
-    tags = Tags.getTags()
-    for key in tags.keys():
+
+def extract_tags(text: str) -> list:
+    if text is None:
+        return []
+    tags = Tags.get_tags()
+    for _ in tags.keys():
         return [tags[key] for key in tags.keys() if key.casefold() in text.casefold()]
 
-def print_transactions(input, export_format=None):
+
+def print_transactions(text, export_format=None):
+    data = json.loads(json.dumps(text))
     if export_format == EXPORT_FORMAT.JSON:
-        print(json.dumps(input))
+        print(data)
     elif export_format == EXPORT_FORMAT.TABLE:
         pd.set_option('display.max_rows', None)
-        print(pd.DataFrame(json.loads(json.dumps(input))))
+        print(pd.DataFrame(data))
     elif export_format == EXPORT_FORMAT.CSV:
         # Create a Pandas Excel writer using XlsxWriter as the engine.
-        data = json.loads(json.dumps(input))
         sheet_name = data[0]["tx_date"][-4:]
         timestamp = f"{datetime.now():%Y%m%dT%H%M%S}"
         filename = f"{OUTPUT_DIRECTORY}/{timestamp}.xlsx"
         writer = pd.ExcelWriter(filename, engine='xlsxwriter')
-        pd.DataFrame(data).to_excel(writer,sheet_name=sheet_name)
+        pd.DataFrame(data).to_excel(writer, sheet_name=sheet_name)
         writer.save()
         print(f"Created: {filename}")
