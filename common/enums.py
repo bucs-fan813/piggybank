696d12fda94fab4dba51638a1dd8af34b4944258
John Russell <jrussell@casteliasystems.com>
2022-04-11 11:45:56 -0400

    added EXPORT_PERIOD to enums.py

--- common/enums.py
+++ common/enums.py
@@ -1,5 +1,6 @@
 import enum
 
+
 class EXPORT_FORMAT(str, enum.Enum):
     """Enums for export formats
     """
@@ -8,8 +9,18 @@ class EXPORT_FORMAT(str, enum.Enum):
     TABLE = "table"
     CSV = "csv"
 
+
+class EXPORT_PERIOD(str, enum.Enum):
+    """Enums for import providers
+    """
+    MONTHLY = 1
+    YEARLY = 2
+    STATEMENT = 3
+    ALL = 4
+
+
 class IMPORT_PROVIDER(str, enum.Enum):
-    """Enums for export formats
+    """Enums for import providers
     """
     Y2020 = "CrawfordTech"
     Y2021 = "iText"
